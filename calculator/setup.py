4c6ea0a3ab132ce409df80fcb485f96b425552dc
Moriwam <mariwamimi99@gmail.com>
2022-10-25 23:04:01 +0600

    setup added

---
+++ calculator/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name='calculator',
+    version='0.1',
+    author="Moriwam",
+    author_email="mariwammimi99@gmail.com",
+    description="<Basic desc>",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
